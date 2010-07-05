8b02bed3f9e64270a462e37988c684e999b0bbf9
tjgolden <tjgolden@e265957e-b7df-11dd-a503-ed2d4bea8bb5>
2010-07-05 15:36:19 +0000

    Little bit more py3-ifying on untested modules
    modified   environment.py

--- winsys/environment.py
+++ winsys/environment.py
@@ -1,5 +1,5 @@
 # -*- coding: iso-8859-1 -*-
-ur"""Each process has an environment block (which may be empty). It
+r"""Each process has an environment block (which may be empty). It
 consists of a set of key-value pairs, each of which is a string.
 The value string may be formed partly or wholly from other environment
 variables using the %envvar% notation. By default, this module will
@@ -13,9 +13,9 @@ system to reflect the user's profile location and home drive etc.
 All three environments are available as a dictalike class whose
 interface matches the :class:`Env` base class. Each environment
 object quacks like a dict in respect of item access, :meth:`Env.get`,
-:meth:`Env.keys`, :meth:`Env.items` and :meth:`Env.update` methods 
-and the system and user objects supply an additional :meth:`Persistent.broadcast` 
-method which sends a message to top-level windows, such as the shell, to 
+:meth:`Env.keys`, :meth:`Env.items` and :meth:`Env.update` methods
+and the system and user objects supply an additional :meth:`Persistent.broadcast`
+method which sends a message to top-level windows, such as the shell, to
 indicate that the environment has changed.
 """
 import os, sys
@@ -36,118 +36,118 @@ WINERROR_MAP = {
 wrapped = exc.wrapper (WINERROR_MAP, x_environment)
 
 class _DelimitedText (list):
-  ur""""Helper class for values such as PATH and PATHEXT which are
+  r""""Helper class for values such as PATH and PATHEXT which are
   consistently semicolon-delimited text but which can helpfully
   be treated as a list of individual values. Subclasseed from
   list, it keeps track of the delimited list while exposing
   the more familiar Pythonesque list interface.
   """
-  
-  def __init__ (self, env, key, delimiter=u";", initialiser=[]):
+
+  def __init__ (self, env, key, delimiter=";", initialiser=[]):
     super (_DelimitedText, self).__init__ (initialiser or env[key].split (delimiter))
     self.env = env
     self.key = key
     self.delimiter = unicode (delimiter)
-  
+
   def _update (self):
     self.env[self.key] = self.delimiter.join (self)
 
   def __delitem__ (self, *args):
     super (_DelimitedText, self).__delitem__ (*args)
     self._update ()
-  
+
   def __delslice__ (self, *args):
     super (_DelimitedText, self).__delslice__ (*args)
     self._update ()
-  
+
   def __iadd__ (self, iterator):
     super (_DelimitedText, self).__iadd__ (self.munge_item (unicode (i)) for i in iterator)
     self._update ()
     return self
-  
+
   def __setitem__ (self, index, item):
     super (_DelimitedText, self).__setitem__ (index, self.munge_item (unicode (item)))
     self._update ()
-  
+
   def __setslice__ (self, index0, index1, iterator):
     super (_DelimitedText, self).__setitem__ (index0, index1, (self.munge_item (unicode (item)) for item in iterator))
     self._update ()
-  
+
   def append (self, item):
     super (_DelimitedText, self).append (self.munge_item (unicode (item)))
     self._update ()
-    
+
   def extend (self, item):
     super (_DelimitedText, self).extend (self.munge_item (unicode (item)))
     self._update ()
-  
+
   def insert (self, index, item):
     super (_DelimitedText, self).insert (index, self.munge_item (unicode (object)))
     self._update ()
-  
+
   def pop (self, index=-1):
     result = super (_DelimitedText, self).pop (index)
     self._update ()
     return result
-  
+
   def remove (self, item):
     super (_DelimitedText, self).remove (self.munge_item (unicode (item)))
     self._update ()
-  
+
   def reverse (self):
     super (_DelimitedText, self).reverse ()
     self._update ()
-  
+
   def sort (self):
     super (_DelimitedText, self).sort ()
     self._update ()
-  
+
   def munge_item (self, item):
     return item
 
 class _DelimitedPath (_DelimitedText):
-  ur"""Subclass of delimited text to ensure that valid filesystem paths
+  r"""Subclass of delimited text to ensure that valid filesystem paths
   are stored in the env var
   """
-  
+
   def munge_item (self, item):
     return os.path.normpath (item).rstrip ("\\")
 
 class Env (core._WinSysObject):
-  ur"""Semi-abstract base class for all environment classes. Outlines
+  r"""Semi-abstract base class for all environment classes. Outlines
   a dict-like interface which relies on subclasses to implement simple
   :meth:`_get` and :meth:`_items` methods.
-  """  
+  """
   def __getitem__ (self, item):
-    ur"""Get environment strings like dictionary items::
-    
+    r"""Get environment strings like dictionary items::
+
       from winsys import environment
-      
+
       print environment.system ()['windir']
     """
     raise NotImplementedError
-  
+
   def __setitem__ (self, item, value):
-    ur"""Set environment strings like dictionary items::
-    
+    r"""Set environment strings like dictionary items::
+
       from winsys import environment
-      
+
       environment.user ()['winsys'] = 'TEST'
     """
     raise NotImplementedError
-    
+
   def __delitem__ (self, item):
-    ur"""Remove an item from the environment::
-    
+    r"""Remove an item from the environment::
+
       from winsys import environment
-      
+
       del environment.process ()['winsys']
     """
     raise NotImplementedError
-  
+
   def __repr__ (self):
     return repr (dict (self.items ()))
-    
+
   def dumped (self, level):
     return utils.dumped_dict (dict (self.items ()), level)
 
@@ -155,18 +155,18 @@ class Env (core._WinSysObject):
     """Yield environment variable names
     """
     raise NotImplementedError
-  
+
   def items (self, expand=True):
     """Yield key-value pairs of environment variables
-    
+
     :param expand: whether to expand embedded environment variables [True]
     """
-    return (  
-      (k, self.expand (v) if expand else v) 
-        for k, v 
+    return (
+      (k, self.expand (v) if expand else v)
+        for k, v
         in self._items ()
     )
-    
+
   def _get_path (self):
     return _DelimitedPath (self, "PATH")
   def _set_path (self, iterator):
@@ -174,12 +174,12 @@ class Env (core._WinSysObject):
   def _del_path (self):
     del self['PATH']
   path = property (_get_path, _set_path, _del_path)
-  
+
   def get (self, item, default=None, expand=True):
     """Return an environment value if it exists, otherwise
     `[default]`. This is the only way to get an unexpanded
     environment value by setting `expand` to False.
-    
+
     :param item: name of an environment variable
     :param default: value to return if no such environment variable exists.
                     This default is expanded if `expand` is True.
@@ -191,19 +191,19 @@ class Env (core._WinSysObject):
       return default
     else:
       return self.expand (v) if expand else v
-  
+
   def update (self, dict_initialiser):
     """Update this environment from a dict-like object, typically
     another environment::
-    
+
       from winsys import environment
-      
+
       penv = environment.process ()
       penv.update (environment.system ())
     """
     for k, v in dict (dict_initialiser).items ():
       self[k] = v
-      
+
   @staticmethod
   def expand (item):
     """Return a version of `item` with internal environment variables
@@ -211,7 +211,7 @@ class Env (core._WinSysObject):
     by the functions in this class unless you specify `expand=False`.
     """
     return wrapped (win32api.ExpandEnvironmentStrings, unicode (item))
-  
+
 class Process (Env):
   """The environment corresponding to the current process. This is visible
   only to the current process and its children (assuming the environment block
@@ -222,91 +222,91 @@ class Process (Env):
   """
   def __init__ (self):
     super (Process, self).__init__ ()
-    
+
   def keys (self):
     return (k for k in wrapped (win32profile.GetEnvironmentStrings).keys ())
-  
+
   def _items (self):
     return (item for item in wrapped (win32profile.GetEnvironmentStrings).items ())
-  
+
   def _get (self, item):
     return wrapped (win32api.GetEnvironmentVariable, item)
-  
+
   def __getitem__ (self, item):
     value = self._get (item)
     if value is None:
       raise KeyError
     else:
       return unicode (value)
-    
+
   def __setitem__ (self, item, value):
     if value is None:
       wrapped (win32api.SetEnvironmentVariable, item, None)
     else:
       wrapped (win32api.SetEnvironmentVariable, item, unicode (value))
-  
+
   def __delitem__ (self, item):
     wrapped (win32api.SetEnvironmentVariable, item, None)
-    
+
 class Persistent (Env):
-  ur"""Represent persistent (registry-based) environment variables. These
+  r"""Represent persistent (registry-based) environment variables. These
   are held at system and at user level, the latter overriding the former
   when an process environment is put together. Don't instantiate this
   class directly: use the :func:`user` and :func:`system` functions.
   """
-  
+
   @staticmethod
   def broadcast (timeout_ms=2000):
-    ur"""Broadcast a message to all top-level windows informing them that
+    r"""Broadcast a message to all top-level windows informing them that
     an environment change has occurred. The message must be sent, not posted,
     and times out after `timeout_ms` ms since some top-level windows handle this
     badly. NB This is a static method.
     """
     win32gui.SendMessageTimeout (
-      win32con.HWND_BROADCAST, win32con.WM_SETTINGCHANGE, 
-      0, "Environment", 
+      win32con.HWND_BROADCAST, win32con.WM_SETTINGCHANGE,
+      0, "Environment",
       win32con.SMTO_ABORTIFHUNG, timeout_ms
     )
 
   def __init__ (self, root):
     super (Persistent, self).__init__ ()
     self.registry = registry.registry (root)
-    
+
   def _get (self, item):
     try:
       return unicode (self.registry.get_value (item))
     except exc.x_not_found:
       return None
-  
+
   def keys (self):
     return (name for name, value in self.registry.values ())
-  
+
   def _items (self):
     return self.registry.values ()
-  
+
   def __getitem__ (self, item):
     value = self._get (item)
     if value is None:
       raise KeyError
     else:
       return value
-    
+
   def __setitem__ (self, item, value):
     self.registry.set_value (item, unicode (value))
-    
+
   def __delitem__ (self, item):
     del self.registry[item]
-    
+
 def process ():
-  ur"""Return a dict-like object representing the environment block of the
+  r"""Return a dict-like object representing the environment block of the
   current process.
   """
   return Process ()
 
 def system (machine=None):
-  ur"""Return a dict-like object representing the system-level persistent
+  r"""Return a dict-like object representing the system-level persistent
   environment variables, optionally selecting a different machine.
-  
+
   :param machine: name or address of a different machine whose system
                   environment is to be represented.
   """
@@ -318,10 +318,10 @@ def system (machine=None):
   return Persistent (root)
 
 def user ():
-  ur"""Return a dict-like object representing the user-level persistent
+  r"""Return a dict-like object representing the user-level persistent
   environment for the logged-on user.
-  
+
   TODO: include alternate user functionality via logon token
   """
-  return Persistent (ur"HKCU\Environment")
+  return Persistent (r"HKCU\Environment")
 
