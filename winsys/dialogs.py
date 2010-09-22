3fd02afe2b8c1882bab07934918798b1609f79d9
tjgolden <tjgolden@e265957e-b7df-11dd-a503-ed2d4bea8bb5>
2010-09-22 15:39:31 +0000

    Force int for coords
    modified   dialogs.py

--- winsys/dialogs.py
+++ winsys/dialogs.py
@@ -447,8 +447,13 @@ class Dialog (BaseDialog):
     r = min (r, l + self.MAX_W)
 
     dt_l, dt_t, dt_r, dt_b = wrapped (win32gui.GetWindowRect, parent)
-    centre_x, centre_y = wrapped (win32gui.ClientToScreen, parent, (int (round ((dt_r - dt_l) / 2)), int (round ((dt_b - dt_t) / 2))))
-    wrapped (win32gui.MoveWindow, self.hwnd, round (centre_x - (r / 2)), round (centre_y - (b / 2)), r - l, b - t, 0)
+
+    cx = int (round ((dt_r - dt_l) / 2))
+    cy = int (round ((dt_b - dt_t) / 2))
+    centre_x, centre_y = wrapped (win32gui.ClientToScreen, parent, (cx, cy))
+    dx = int (round (centre_x - (r / 2)))
+    dy = int (round (centre_y - (b / 2)))
+    wrapped (win32gui.MoveWindow, self.hwnd, dx, dy, r - l, b - t, 0)
     l, t, r, b = wrapped (win32gui.GetClientRect, self.hwnd)
     self._resize (r - l, b - t, 0)
     return True
