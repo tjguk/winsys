4d2cb3accb62d1c1e0f4d616c9b632ac49a3c407
Tim Golden <mail@timgolden.me.uk>
2014-01-23 10:04:53 +0000

    PEP8

--- winsys/_security/core.py
+++ winsys/_security/core.py
@@ -5,8 +5,8 @@ import win32security
 
 from winsys import constants
 
-REVISION = constants.Constants.from_list ([
-  "ACL_REVISION",
-  "ACL_REVISION_DS",
-  "SDDL_REVISION_1"
+REVISION = constants.Constants.from_list([
+    "ACL_REVISION",
+    "ACL_REVISION_DS",
+    "SDDL_REVISION_1"
 ], namespace=win32security)
