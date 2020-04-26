d7134800902a6d27004a92d010edef0c0b94e9e5
Tim Golden <mail@timgolden.me.uk>
2020-04-26 10:56:36 +0100

    Bump the version

--- winsys/__init__.py
+++ winsys/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = '1.1'
+__version__ = '1.2'
 __title__ = "winsys"
 __description__ = "WinSys: the Python Windows Administrator's Toolkit"
 __author__ = "Tim Golden"
